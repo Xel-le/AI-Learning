8fdd3b9c9f0b93b258a505105dd775e004180dcb
Xel-le <dergunov_svyatoslav@protonmail.com>
2026-05-31 16:09:21 +0500

    Fouth commit

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
+# My first Python program
 name = input("What is your name? ")
-print("Hello, "+name+"!")
+print("Hello,", sep ="", end=" ")
+print(name, sep ="", end="")
+print("!", sep ="", end="")
