133cf93f7aac29e6937bff9ba2fa70b6e50ea1de
Xel-le <dergunov_svyatoslav@protonmail.com>
2026-06-01 12:30:53 +0500

    Week 0 improvements

--- einstein.py
+++ einstein.py
@@ -2,6 +2,6 @@ c = 300000000
 
 def main ():
     m = int(input("Mass: "))
-    print("Energy: " + str(m * c ** 2))
+    print(f"Energy: {m*c**2}")
 
 main()
--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 def main():
     fullName = input("Input your full name: ").strip().title()
     if " " in fullName:
-        firstName, lastName = fullName.split(" ")
-        if (firstName == "Arnela" or firstName == "Arnella" or firstName == "Ornela" or firstName == "Ornella") and lastName == "Loktionova":
+        firstName, lastName = fullName.split(" ", 1)
+        if firstName in {"Arnela", "Arnella", "Ornela", "Ornella"} and lastName == "Loktionova":
             print(f"I love you, {firstName} <3")
         else:
             print(f"Hello, {fullName}!")
