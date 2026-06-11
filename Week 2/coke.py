8534cb9a11d84e6008020822c6b52289df9e0461
Xel-le <dergunov_svyatoslav@protonmail.com>
2026-06-12 00:26:35 +0500

    Week 2 Impr

--- Week 2/coke.py
+++ Week 2/coke.py
@@ -2,12 +2,11 @@ def main():
     amount_due = int(50)
     print(f"Amount Due: {amount_due}")
     while amount_due > 0:
-        while True:
-            insert_amount  = int(input("Insert Coin: "))
-            if insert_amount in [5, 10, 25]:
-                break
+        insert_amount  = int(input("Insert Coin: "))
+        if insert_amount in [5, 10, 25]:
+            amount_due = amount_due - insert_amount
+            print(f"Amount Due: {amount_due}") if amount_due > 0 else print(f"Change Owed: {0-amount_due}")
+        else:
             print(f"Amount Due: {amount_due}")
-        amount_due = amount_due - insert_amount
-        print(f"Amount Due: {amount_due}") if amount_due >= 0 else print(f"Change Owed: {0-amount_due}")
 
 main()
