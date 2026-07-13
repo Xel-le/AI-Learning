b2345ad7b1eec6abd2f8eb47322ddeb584ae78d3
Xel-le <dergunov_svyatoslav@protonmail.com>
2026-07-13 16:09:36 +0500

    Week 4

--- Week 4/fonts.py
+++ Week 4/fonts.py
@@ -3,21 +3,24 @@ import pyfiglet
 import random
 
 def main():
-    if len(sys.argv) > 3:
+    if len(sys.argv) == 1:
+        font=random.choice(pyfiglet.FigletFont.getFonts())
+    elif len(sys.argv) > 3:
         sys.exit("Too many arguments")
-    elif len(sys.argv) < 2:
+    elif len(sys.argv) < 3:
         sys.exit("Too few arguments")
     elif sys.argv[1] != '-f' and sys.argv[1] != '--font':
         sys.exit("Invalid usage")
     else:
-        text = input("Input: ").strip()
-        try:
-            if sys.argv[2] in pyfiglet.FigletFont.getFonts():
-                print(pyfiglet.figlet_format(text, font=sys.argv[2]))
-            else:
-                sys.exit("Invalid usage")
-        except IndexError:
-            print(pyfiglet.figlet_format(text, font=random.choice(pyfiglet.FigletFont.getFonts())))
+        font=sys.argv[2]
+    text = input("Input: ").strip()
+    try:
+        if sys.argv[2] in pyfiglet.FigletFont.getFonts():
+            print(pyfiglet.figlet_format(text, font))
+        else:
+            sys.exit("Invalid font")
+    except IndexError:
+        print(pyfiglet.figlet_format(text, font))
 
 
 main()
--- Week 4/professor.py
+++ Week 4/professor.py
@@ -8,6 +8,7 @@ def main():
     selected_level = get_level()
     x, y = generate_integer(selected_level)
     for i in range(10):
+        errors = 0
         while True:
             try:
                 answer = int(input(f"{x[i]} + {y[i]} = "))
@@ -18,7 +19,6 @@ def main():
                     errors += 1
                     print("EEE")
                 elif answer != (x[i] + y[i]) and errors == 2:
-                    errors = 0
                     print("EEE")
                     print(f"{x[i]} + {y[i]} = {x[i] + y[i]}")
                     break
@@ -49,9 +49,14 @@ def get_level():
 def generate_integer(l):
     x_func = []
     y_func = []
-    for _ in range(10):
-        x_func.append(random.randrange((10**(l-1)),(10**l)))
-        y_func.append(random.randrange((10**(l-1)),(10**l)))
+    if l == 1:
+        for _ in range(10):
+            x_func.append(random.randrange(0,(10**l)))
+            y_func.append(random.randrange(0,(10**l)))
+    else:
+        for _ in range(10):
+            x_func.append(random.randrange((10**(l-1)),(10**l)))
+            y_func.append(random.randrange((10**(l-1)),(10**l)))
     return x_func, y_func
 
 
